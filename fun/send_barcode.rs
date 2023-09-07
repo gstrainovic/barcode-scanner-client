5b00d87cd479629a4464bb4053cadc4775a96d92
gstrainovic <g.strainovic@gmail.com>
2023-09-07 21:14:24 +0200

    barcode lokal statt server senden

--- fun/send_barcode.rs
+++ fun/send_barcode.rs
@@ -4,6 +4,15 @@ use req::write_barcode::write_barcode;
 
 pub fn send_barcode(barcode: String, user: i32, jwt: &str, lager_user_ids: Vec<i32>) {
     let barcode_c = barcode.clone();
+
+    if jwt.is_empty() {
+        Notification::new()
+        .summary(&format!("Barcode Scanner: {} lokal gespeichert", barcode_c))
+        .show()
+        .unwrap();
+        return;
+    }
+
     match write_barcode(barcode, user, jwt, lager_user_ids) {
         Ok(_) => {
             Notification::new()

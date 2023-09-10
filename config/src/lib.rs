f4304d6e31df216d1bfada1f6fa3e7abb8f831c5
gstrainovic <g.strainovic@gmail.com>
2023-09-10 17:35:12 +0200

    v1.1.7

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -1,4 +1,4 @@
 // pub const STRAPI_URL: &str = "http://146.190.19.207:1337"; // the old server
-// pub const STRAPI_URL: &str = "http://167.235.68.5:1337"; // the new server
-pub const STRAPI_URL: &str = "http://localhost:1337";  // dev server 
-pub const VERSION : &str = "1.1.6";
+pub const STRAPI_URL: &str = "http://167.235.68.5:1337"; // the new server
+// pub const STRAPI_URL: &str = "http://localhost:1337";  // dev server 
+pub const VERSION : &str = "1.1.7";

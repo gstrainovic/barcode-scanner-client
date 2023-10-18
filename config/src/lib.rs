18c5ea37e114eb85422c97fa28002776c87de69c
gstrainovic <g.strainovic@gmail.com>
2023-10-18 21:52:54 +0200

    1.1.9

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -1,3 +1,3 @@
 pub const STRAPI_URL: &str = "http://167.235.68.5:1337"; // the new server
 // pub const STRAPI_URL: &str = "http://localhost:1337";  // dev server 
-pub const VERSION : &str = "1.1.8";
+pub const VERSION : &str = "1.1.9";

7fab3b5e9f921677d3544ab4aaa0c8298e7238b6
gstrainovic <g.strainovic@gmail.com>
2023-11-01 19:31:02 +0100

    only alphanumeric characters!

--- fun/process_barcode.rs
+++ fun/process_barcode.rs
@@ -51,15 +51,14 @@ pub fn process_barcode(
     history: fltk::browser::HoldBrowser,
 ) {
     i.activate();
-    let barcode = i.value();
-    let barcode_c = barcode.clone();
+    let mut barcode_new = i.value();
+    // let barcode_c = barcode.clone();
     i.set_value("");
 
-    
-    // remove barcode all non-ascii, whitespace characters and make it lowercase
-    let barcode_lower = barcode_c
+    // remove from barcode all characters that are not alphanumeric and make it lowercase
+    barcode_new = barcode_new
         .chars()
-        .filter(|c| c.is_ascii() && !c.is_whitespace())
+        .filter(|c| c.is_alphanumeric())
         .collect::<String>()
         .to_lowercase();
 
@@ -93,11 +92,11 @@ pub fn process_barcode(
 
         // if barcode ends with a string from barcode_ausnahmen, then send it directly to server
         for barcode_ausnahme in ausnahmen {
-            if barcode_lower.ends_with(barcode_ausnahme.Barcode.to_lowercase().as_str()) {
-                send_barcode(barcode_c.clone(), user_id, &jwt, &lager_user_ids);
+            if barcode_new.ends_with(barcode_ausnahme.Barcode.to_lowercase().as_str()) {
+                send_barcode(barcode_new.clone(), user_id, &jwt, &lager_user_ids);
                 history_add(
                     errors::ausnahme(barcode_ausnahme.Bedeutung),
-                    &barcode_c,
+                    &barcode_new,
                     history,
                     user_id,
                     offline,
@@ -108,17 +107,17 @@ pub fn process_barcode(
         }
     }
 
-    if barcode_lower.len() < settings.Barcode_Mindestlaenge as usize {
+    if barcode_new.len() < settings.Barcode_Mindestlaenge as usize {
         Notification::new()
             .summary(&format!(
                 "Barcode Scanner: {} ist zu kurz, nicht gesendet",
-                barcode_c
+                barcode_new
             ))
             .show()
             .unwrap();
         history_add(
             errors::zu_kurz(),
-            &barcode_c,
+            &barcode_new,
             history,
             user_id,
             offline,
@@ -146,7 +145,7 @@ pub fn process_barcode(
 
         for idatr in leitcodes {
             let attribute: Leitcode = idatr.attributes;
-            if barcode_lower.len() > attribute.Mindeslaenge as usize {
+            if barcode_new.len() > attribute.Mindeslaenge as usize {
                 let beschreibung = attribute.Beschreibung;
                 let data_buchstaben: Vec<IdAtrBuchstaben> = attribute.Leitcode_Buchstabe.data;
                 for buchstabe_atr_id in data_buchstaben {
@@ -154,21 +153,21 @@ pub fn process_barcode(
                     let position: usize = buchstabe_attr.Position_Null_Beginnend as usize;
 
                     // does the barcode match witch buchstabe at position?
-                    println!("barcode_lower{:?}", barcode_lower);
-                    if barcode_lower.len() > position {
-                        let barcode_buchstabe = barcode_lower.chars().nth(position).unwrap();
+                    println!("barcode_lower{:?}", barcode_new);
+                    if barcode_new.len() > position {
+                        let barcode_buchstabe = barcode_new.chars().nth(position).unwrap();
                         println!("barcode_buchstabe{:?}", barcode_buchstabe);
                         if buchstabe_attr.Buchstabe == barcode_buchstabe.to_string() {
                             Notification::new()
                                 .summary(&format!(
                                     "Barcode Scanner: {} als {} erkannt, nicht gesendet",
-                                    barcode_c, beschreibung
+                                    barcode_new, beschreibung
                                 ))
                                 .show()
                                 .unwrap();
                             history_add(
                                 errors::leitcode(beschreibung),
-                                &barcode_c,
+                                &barcode_new,
                                 history,
                                 user_id,
                                 offline,
@@ -185,17 +184,17 @@ pub fn process_barcode(
     let mut is_barcode_duplicate_bool = false;
 
     if offline {
-        is_barcode_duplicate_bool = is_barcode_duplicate_sqlite(&barcode_c);
+        is_barcode_duplicate_bool = is_barcode_duplicate_sqlite(&barcode_new);
     } else {
-        is_barcode_duplicate_bool = is_barcode_duplicate(&jwt, &barcode_c, &user_id).unwrap();
+        is_barcode_duplicate_bool = is_barcode_duplicate(&jwt, &barcode_new, &user_id).unwrap();
     }
 
     if !is_barcode_duplicate_bool {
-        send_barcode(barcode_lower.clone(), user_id, &jwt, lager_user_ids);
+        send_barcode(barcode_new.clone(), user_id, &jwt, lager_user_ids);
 
         // does the barcode contain a number?
         let mut contains_number = false;
-        for c in barcode_lower.chars() {
+        for c in barcode_new.chars() {
             if c.is_numeric() {
                 contains_number = true;
                 break;
@@ -208,19 +207,19 @@ pub fn process_barcode(
             errors::no_numbers()
         };
 
-        history_add(err, &barcode_c, history, user_id, offline, lager_user_ids);
+        history_add(err, &barcode_new, history, user_id, offline, lager_user_ids);
     } else {
         Notification::new()
             .summary(&format!(
                 "Barcode Scanner: {} wurde bereits gesendet",
-                barcode_c
+                barcode_new
             ))
             .show()
             .unwrap();
 
         history_add(
             errors::bereits_gesendet(),
-            &barcode_c,
+            &barcode_new,
             history,
             user_id,
             offline,

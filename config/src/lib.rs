7f766f24f945353e1d5dd0af75ff3ce2ac71a536
goran <goran.strainovic@traffitec.ch>
2023-12-14 22:50:58 +0100

    new server url

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -1,3 +1,3 @@
-pub const STRAPI_URL: &str = "http://167.235.68.5:1337"; // the new server
+pub const STRAPI_URL: &str = "https://gz-strapi.strainovic-it.ch"; // the new server
 // pub const STRAPI_URL: &str = "http://localhost:1337";  // dev server 
-pub const VERSION : &str = "1.2.1";
+pub const VERSION : &str = "1.2.2";
